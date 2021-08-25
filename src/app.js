4a5e76401b8e7dd1e695a63eae98ca5d1dade008
RailonA <railonacosta@gmail.com>
2021-08-25 11:30:43 -0400

    Fix the set up

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <div>
+        <p>
+          Hellow World!
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
