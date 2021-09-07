925d33aee61e8d6355c5f95ef2b8ef1e28605d51
RailonA <railonacosta@gmail.com>
2021-09-07 17:26:34 -0400

    Try fixing list

--- src/Components/filterForm.js
+++ src/Components/filterForm.js
@@ -3,22 +3,27 @@ import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 import '../styles/filterForm.css';
 
-const BreedFilter = ({ dogBreed }) => {
+const FilterForm = ({ onBreedToFilter }) => {
   const breedsData = useSelector((state) => state.breedReducer);
   const options = breedsData.map((breed) => (
-    <option value={breed} key={breed}>
+    <option value={breed.id} key={breed.id}>
       {breed.name}
     </option>
   ));
 
   return (
-    <select onChange={dogBreed}>
+    <select onChange={onBreedToFilter} key={breedsData.id}>
       {options}
     </select>
   );
 };
 
-BreedFilter.propTypes = {
-  dogBreed: PropTypes.func.isRequired,
+FilterForm.propTypes = {
+  onBreedToFilter: PropTypes.func,
 };
-export default BreedFilter;
+
+FilterForm.defaultProps = {
+  onBreedToFilter: () => {
+  },
+};
+export default FilterForm;
